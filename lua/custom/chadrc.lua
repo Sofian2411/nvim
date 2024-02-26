d5f8860825cb77235d2fdfa5318e2a36ad2a3816
sofian.braighith <sofian.braighith@epita.fr>
2024-02-26 08:50:38 +0100

    custom

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,61 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+    theme = 'catppuccin',
+    transparency = false,
+}
+
+M.plugins = "custom.plugins"
+M.mappings = require("custom.mappings")
+
+-- Vim options {{{
+vim.o.incsearch = true
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ttimeoutlen = 50
+vim.o.cc = "80"
+vim.o.sw = 4
+vim.o.sts = 4
+vim.o.ts = 4
+vim.o.expandtab = true
+vim.o.smarttab = true
+vim.o.autoindent = true
+vim.o.cindent = true
+vim.o.wrap = false
+vim.o.foldenable = true
+vim.o.foldmethod = "marker"
+vim.o.hidden = true
+vim.o.undofile = false
+-- }}}
+
+-- Vim script {{{
+vim.cmd([[
+autocmd Filetype c setlocal foldmethod=syntax | set fdl=99
+]])
+-- }}}
+
+-- Mappings {{{
+vim.keymap.set("n", "<C-w>", "<C-w>w", {noremap = true})
+vim.keymap.set("n", "<C-Left>", "<C-w>H", {noremap = true})
+vim.keymap.set("n", "<C-Down>", "<C-w>J", {noremap = true})
+vim.keymap.set("n", "<C-Up>", "<C-w>K", {noremap = true})
+vim.keymap.set("n", "<C-Right>", "<C-w>L", {noremap = true})
+vim.keymap.set("n", "<C-H>", "<C-w>h", {noremap = true})
+vim.keymap.set("n", "<C-J>", "<C-w>j", {noremap = true})
+vim.keymap.set("n", "<C-K>", "<C-w>k", {noremap = true})
+vim.keymap.set("n", "<C-L>", "<C-w>l", {noremap = true})
+
+vim.keymap.set("n", "zo", "zO", {noremap = true})
+vim.keymap.set("n", "zc", "zC", {noremap = true})
+vim.keymap.set("n", "zr", "zR", {noremap = true})
+vim.keymap.set("n", "zm", "zM", {noremap = true})
+
+vim.keymap.set("i", "jj", "<Esc>", {noremap = true})
+vim.keymap.set("n", "<silent> <leader>f", ":Format<CR>", {noremap = true})
+vim.keymap.set("n", "<silent> <leader>F", ":FormatWrite<CR>", {noremap = true})
+-- }}}
+
+return M
---
+++ lua/custom/configs/formatter.lua
@@ -0,0 +1,48 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+local opts = {
+    -- Enable or disable logging
+    logging = true,
+    -- Set the log level
+    log_level = vim.log.levels.WARN,
+    -- All formatter configurations are opt-in
+    filetype = {
+        cpp = {
+            function()
+                return {
+                    exe = "clang-format",
+                    args = {
+                        "-i",
+                    },
+                }
+            end
+        },
+
+        c = {
+            function()
+                return {
+                    exe = "clang-format",
+                    args = {
+                        "-i",
+                    },
+                }
+            end
+        },
+        -- Use the special "*" filetype for defining formatter configurations on
+        -- any filetype
+        -- ["*"] = {
+        --   -- "formatter.filetypes.any" defines default configurations for any
+        --   -- filetype
+        --   require("formatter.filetypes.any").remove_trailing_whitespace
+        -- }
+    }
+}
+
+return opts
